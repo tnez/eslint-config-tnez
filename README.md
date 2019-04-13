a1cfb87ea55e6f9b000cfecd23b0d40f6e562f28
Travis Nesland <tnesland@gmail.com>
2019-04-13 06:19:27 -0400

    v0.1.0
    Create initial version of eslint config.

---
+++ README.md
@@ -0,0 +1,29 @@
+# eslint-config-tnez
+
+[![npm version](https://badge.fury.io/js/eslint-config-tnez.svg)](https://badge.fury.io/js/eslint-config-tnez)
+[![GitHub license](https://img.shields.io/github/license/tnez/eslint-config-tnez.svg)](https://github.com/tnez/eslint-config-tnez/blob/master/LICENCE)
+
+## Installation
+
+1. Install all peer dependencies
+
+    - **npm**: `npx install-peerdeps --dev eslint-config-tnez`
+    - **yarn**:  `npx install-peerdeps --dev -Y eslint-config-tnez`
+
+1. Add `.eslintrc`:
+
+    ```
+    {
+      "extends": ["tnez"]
+    }
+    ```
+
+1. (Optionally ) Add `.prettierrc` that matches config in order to play nicely with editor plugins:
+
+    ```
+    {
+      "semi": false,
+      "singleQuote": true,
+      "trailingComma": "all"
+    }
+    ```
---
+++ index.js
@@ -0,0 +1,61 @@
+module.exports = {
+  extends: [
+    'standard',
+    'prettier',
+  ],
+  plugins: [
+    'import',
+    'promise',
+    'jest',
+    'prettier',
+    'standard',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    'jest/globals': true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    // eslint
+    'sort-keys': ['error', 'asc', { caseSensitive: true, natural: true }],
+
+    // eslint-plugin-import
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/prefer-default-export': 0,
+
+    // prettier
+    'prettier/prettier': [
+      'error',
+      // following rules should match .prettierrc
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+
+    // jest
+    'jest/consistent-test-it': [
+      'error',
+      {
+        fn: 'it',
+        withinDescribe: 'it',
+      },
+    ],
+    'jest/expect-expect': 'error',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'warn',
+    'jest/no-identical-title': 'error',
+    'jest/no-jest-import': 'error',
+    'jest/no-large-snapshots': ['warn', { maxSize: 300 }],
+    'jest/prefer-strict-equal': 'warn',
+    'jest/prefer-to-be-null': 'error',
+    'jest/prefer-to-be-undefined': 'error',
+    'jest/prefer-to-have-length': 'error',
+    'jest/valid-expect': 'error',
+  },
+}
