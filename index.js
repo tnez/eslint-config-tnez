48d6252445ebdd60877d6a605e16148bb65cc196
Travis Nesland <tnesland@gmail.com>
2019-07-29 16:09:05 -0400

    fix: syntax in extends

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
